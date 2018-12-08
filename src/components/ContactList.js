722a796982a953f5a956215f45effff478484b18
Ehimah Obuse <ehimah@eroeconsulting.com>
2018-12-08 16:13:52 +0100

    refactored Contact list to use a list of contact items from an array

--- src/components/ContactList.js
+++ src/components/ContactList.js
@@ -2,10 +2,9 @@ import React from 'react';
 import Contact from './Contact';
 
 function ContactList(props) {
-    const contactList = props.contacts.map((c) => <Contact imageUrl={c.imageUrl} name={c.name} time={c.time} />);
+    const contactList = props.contacts.map((c,index) => <Contact key={index} imageUrl={c.imageUrl} name={c.name} time={c.time} />);
     return (
         <div className="row sideBar">
-            {/* <Contact imageUrl="https://placeimg.com/50/50/people" name="Ehimah Obuse" time="17:32 pm" /> */}
             {contactList}
         </div>
     );
