eaa17282e1972ae5162f0fcab7a71f055f597c4a
Ehimah Obuse <ehimah@eroeconsulting.com>
2018-12-10 11:56:29 +0100

    refactored from stateful component to pure react componet

--- src/components/Contact.js
+++ src/components/Contact.js
@@ -1,31 +1,26 @@
 import React from 'react';
 
-class Contact extends React.Component{
-    constructor(props){
-        super(props);
-    }
-
-    render(){
-        return(
-             <div className="row sideBar-body">
-                    <div className="col-sm-3 col-xs-3 sideBar-avatar">
-                        <div className="avatar-icon">
-                            <img src={this.props.imageUrl} />
-                        </div>
+function Contact(props) {
+    return (
+        <div className="row sideBar-body">
+            <div className="col-sm-3 col-xs-3 sideBar-avatar">
+                <div className="avatar-icon">
+                    <img src={props.imageUrl} alt="avatar" />
+                </div>
+            </div>
+            <div className="col-sm-9 col-xs-9 sideBar-main">
+                <div className="row">
+                    <div className="col-sm-8 col-xs-8 sideBar-name">
+                        <span className="name-meta">{props.name}</span>
                     </div>
-                    <div className="col-sm-9 col-xs-9 sideBar-main">
-                        <div className="row">
-                            <div className="col-sm-8 col-xs-8 sideBar-name">
-                                <span className="name-meta">{this.props.name}</span>
-                            </div>
-                            <div className="col-sm-4 col-xs-4 pull-right sideBar-time">
-                                <span className="time-meta pull-right">{this.props.time}</span>
-                            </div>
-                        </div>
+                    <div className="col-sm-4 col-xs-4 pull-right sideBar-time">
+                        <span className="time-meta pull-right">{props.time}</span>
                     </div>
                 </div>
-        );
-    }
+            </div>
+        </div>
+    );
+
 }
 
 export default Contact;
