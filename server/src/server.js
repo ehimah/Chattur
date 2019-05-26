8283d059b5e4181a0e0868fafbbd78ab9b677265
Ehimah Obuse <ehimahobuse@gmail.com>
2019-05-27 00:45:15 +0100

    updates

--- server/src/server.js
+++ server/src/server.js
@@ -1,12 +1,22 @@
 import express from 'express';
 import { json, urlencoded } from 'body-parser';
+import mongoose from 'mongoose'
+
 const app = express();
 const port = process.env.PORT || 5000;
 app.use(json());
 app.use(urlencoded({ extended: true }));
 
+mongoose.Promise = global.Promise;
+mongoose.connect('mongodb://user:password@ds239648.mlab.com:39648/db_name')
+ 
+
 app.get('/api/hello', (req, res) => {
   res.send({ express: 'Hello From Express' });
+});
+
+app.get('api/contacts',(req,res)=>{
+  
 });
 app.post('/api/world', (req, res) => {
   console.log(req.body);
@@ -14,5 +24,7 @@ app.post('/api/world', (req, res) => {
     `I received your POST request. This is what you sent me: ${req.body.post}`,
   );
 });
+
+
 
 app.listen(port, () => console.log(`Listening on port ${port}`));
